e1f259f45b38ce7dcc8849594e3a82da1a5f0354
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-03-15 23:25:56 +0330

    Single instance + open new window on the same process when user tries to run a new instance

--- TabbedShell/App.xaml.cs
+++ TabbedShell/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -15,19 +16,53 @@ namespace TabbedShell
     {
         public List<MainWindow> MainWindows { get; } = new List<MainWindow>();
 
+        // Single instance and notifying the previous instance obtained from https://stackoverflow.com/a/23730146/942659
+
+        private const string UniqueEventName = "428efeec-180d-4406-aa45-d04c71c250fc";
+        private const string UniqueMutexName = "9fddf5c9-a5b8-491a-929a-537ca81ed578";
+        private EventWaitHandle eventWaitHandle;
+        private Mutex mutex;
+
+        /// <summary>The app on startup.</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
         protected override void OnStartup(StartupEventArgs e)
         {
-            if (e.Args.Length > 0)
+            bool isOwned;
+            this.mutex = new Mutex(true, UniqueMutexName, out isOwned);
+            this.eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+
+            // So, R# would not give a warning that this variable is not used.
+            GC.KeepAlive(this.mutex);
+
+            if (isOwned)
             {
-                if (e.Args[0].StartsWith("tabbedshell:"))
-                {
-                    var activationUrl = e.Args[0];
+                // Spawn a thread which will be waiting for our event
+                var thread = new Thread(
+                    () =>
+                    {
+                        while (this.eventWaitHandle.WaitOne())
+                        {
+                            Current.Dispatcher.BeginInvoke(
+                                (Action)(() =>
+                                {
+                                    (new MainWindow()).Show();
+                                }));
+                        }
+                    });
 
+                // It is important mark it as background otherwise it will prevent app from exiting.
+                thread.IsBackground = true;
 
-                }
+                thread.Start();
+                return;
             }
-            
-            base.OnStartup(e);
+
+            // Notify other instance so it could bring itself to foreground.
+            this.eventWaitHandle.Set();
+
+            // Terminate this instance.
+            this.Shutdown();
         }
     }
 }
