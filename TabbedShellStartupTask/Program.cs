7bb8c9886f42048689b1153ba720c2d933f65df9
Mahdi Ghiasi <mghiasi75@gmail.com>
2019-03-17 22:06:49 +0330

    Startup task project

---
+++ TabbedShellStartupTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabbedShellStartupTask
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Process.Start("TabbedShell.exe", "--startup");
+        }
+    }
+}
